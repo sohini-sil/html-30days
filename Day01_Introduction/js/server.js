6b7f247c339e925fdf59b44afdf9aa32464b0f36
Sohini Sil <sohini.sil@outlook.com>
2026-09-04 23:00:52 +0530

    day 11

--- Day01_Introduction/js/server.js
+++ Day01_Introduction/js/server.js
@@ -11,3 +11,9 @@ server.listen(3000, '127.0.0.1', () => {
 });
  
 const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from my Node.js server!');
+}
